6c6d447749cf3aac2170610d6bd0beb21fcb67d5
macstudent <macstudent@10.10.150.51>
2019-05-14 11:41:57 -0400

    second commit

--- tests/InputBoxTest.java
+++ tests/InputBoxTest.java
@@ -3,6 +3,10 @@ import static org.junit.Assert.*;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class InputBoxTest {
 
@@ -16,7 +20,33 @@ public class InputBoxTest {
 
 	@Test
 	public void testSingleInputField() {
-		fail("Not yet implemented");
+//		fail("Not yet implemented");
+		
+		System.setProperty("webdriver.chrome.driver","/Users/macstudent/Desktop/chromedriver");
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://www.seleniumeasy.com/test/basic-first-form-demo.html");
+		
+//		1. Find the textbox.   (id = user-message)
+//		2. Type "Hello World" into textbox.
+//		3. Find the Button.		(????)
+//		4. Click on the Button
+//		5. Find the output msg thing.	(id = display)
+//		6. check the output message.	(expected result = "Hello World")
+		
+		WebElement textBox = driver.findElement(By.id("user-message"));
+		textBox.sendKeys("Hello World");
+		
+		WebElement button = driver.findElement(By.cssSelector("form#get-input button"));
+		button.click();
+		
+		WebElement outputSpan = driver.findElement(By.id("display"));
+		String outputMessage = outputSpan.getText();
+		
+		assertEquals("Hello World", outputMessage);
+		
+		driver.close();
+		
 	}
 	
 	public void testTwoInputFields() {
