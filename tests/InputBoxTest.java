9c60acdd65623a2112a8afc5d96624a0e4ba3b05
macstudent <macstudent@10.10.150.51>
2019-05-14 10:34:23 -0400

    Initial commit

---
+++ tests/InputBoxTest.java
@@ -0,0 +1,26 @@
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class InputBoxTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testSingleInputField() {
+		fail("Not yet implemented");
+	}
+	
+	public void testTwoInputFields() {
+		
+	}
+
+}
